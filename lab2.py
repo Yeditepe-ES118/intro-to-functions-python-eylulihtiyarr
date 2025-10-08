3c86bc854d7fac49a05938a14ec171288967e201
emelildeniz <emel.ildeniz@std.yeditepe.edu.tr>
2025-10-08 15:25:06 +0300

    add a function called find_circumference

---
+++ lab2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  8 14:56:38 2025
+
+@author: user
+"""
+
+def find_cirumference(a,b):
+    c= 2*a + 2*b 
+    return c 
+
+myresult = find_cirumference(1.9,3.3)
+
+
+
+
